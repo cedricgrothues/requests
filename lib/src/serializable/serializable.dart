3c516dcef86b9cabd32a0ebf567657bd09947839
Cedric Grothues <cedricgrothues@gmail.com>
2021-07-12 09:43:40 +0200

    Update serializable.dart

--- lib/src/serializable/serializable.dart
+++ lib/src/serializable/serializable.dart
@@ -1,9 +1,11 @@
-import 'package:meta/meta.dart' show immutable;
+import 'package:meta/meta.dart' show immutable, internal, nonVirtual;
 
 ///
 @immutable
 abstract class Serializable<T> {
   ///
+  @internal
+  @nonVirtual
   final Type type = T;
 
   ///
